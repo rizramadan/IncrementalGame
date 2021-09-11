4cfc425103c363aa0e0159f0267aff2b12f9360a
rizramadan <rizki123304@gmail.com>
2021-09-11 13:40:40 +0700

    Icremental Game

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public AudioSource tapSound;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        tapSound.PlayOneShot(tapSound.clip);
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+    }
+}
